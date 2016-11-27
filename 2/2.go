a36d12ab02679b9c9202e4999a3c8da93daa3716
MClode <martin@workdigital.co.uk>
2016-11-27 21:01:03 +0000

    Euler problem 2 in Golang, simple recursive solution

---
+++ 2/2.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	print(fibonacci(0,1,0), "\n")
+}
+
+func fibonacci (x, y, total int) int {
+	if ((x + y) % 2 == 0) {
+		total += x + y
+	}
+
+	if (x + y + y > 4000000) {
+		return total
+	}
+
+	return fibonacci(y, x+y, total)
+}
