707a8e36fe679a34ff11eac7a8f5bc7157baa661
MClode <martin@workdigital.co.uk>
2016-11-28 12:56:36 +0000

    Brute force solution to problem 3, unoptimised

---
+++ 3/3.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	print(factor(600851475143))
+}
+
+func factor(num int) int {
+	var f = 2
+
+	for (num > f) {
+		if (num % f == 0) {
+			num = num / f
+			f = 2
+		} else {
+			f++
+		}
+	}
+	return f
+}
