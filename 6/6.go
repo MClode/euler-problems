ab50ae9958476d601318e419a4a98ff894308930
MClode <martin@workdigital.co.uk>
2016-11-30 10:59:06 +0000

    naive solution to problem 6

---
+++ 6/6.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+	x := sumOfSquares(1,0)
+	y := squareOfSum(1,0)
+
+	print(y-x)
+}
+
+func sumOfSquares(num, sum int) int {
+	if (num > 100) {
+		return sum
+	}
+	square := num * num
+	return sumOfSquares(num+1, square+sum)
+}
+
+func squareOfSum(num, sum int) int {
+	if (num > 100) {
+		return sum * sum
+	}
+	return squareOfSum(num+1, sum+num)
+}
